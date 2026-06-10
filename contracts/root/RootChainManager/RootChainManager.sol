bd4060ecbb6f415e0dafe05d5396ae80ea76be31
aaron.liu <aaron.liu@tron.network>
2026-06-10 16:10:37 +0800

    feat: keep align with ETHER_ADDRESS

--- contracts/root/RootChainManager/RootChainManager.sol
+++ contracts/root/RootChainManager/RootChainManager.sol
@@ -40,7 +40,7 @@ contract RootChainManager is
     address public constant ETHER_ADDRESS = 0xff00000000000000000000000000000000000002;
     bytes32 public constant MAPPER_ROLE = keccak256("MAPPER_ROLE");
     bytes32 public constant CFO_ROLE = keccak256("CFO_ROLE");
-    uint64 public constant CHAIN_ID = 2;  // 1: tron   2: eth  3: bsc
+    uint64 public constant CHAIN_ID = 3;  // 1: tron   2: eth  3: bsc
 
     function _msgSender()
         internal
